e18c08e9fc7fc33f155b8ab45f58f553b85455a6
AndreasLonn <45557341+AndreasLonn@users.noreply.github.com>
2021-11-02 19:40:36 +0100

    Add Notes PWA

---
+++ notes/serviceworker.js
@@ -0,0 +1,46 @@
+const cacheName = 'notes-v1';
+const appShellFiles = [
+    '/notes/',
+    '/notes/index.html',
+    '/notes/favicon.ico',
+    '/notes/icons/icon-32.png',
+    '/notes/icons/icon-64.png',
+    '/notes/icons/icon-96.png',
+    '/notes/icons/icon-128.png',
+    '/notes/icons/icon-168.png',
+    '/notes/icons/icon-192.png',
+    '/notes/icons/icon-256.png',
+    '/notes/icons/icon-512.png',
+];
+const contentToCache = appShellFiles;
+
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install');
+    e.waitUntil((async() => {
+        const cache = await caches.open(cacheName);
+        console.log('[Service Worker] Caching all: app shell and content');
+        await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith((async() => {
+        const r = await caches.match(e.request);
+        console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+        if (r) { return r; }
+        const response = await fetch(e.request);
+        const cache = await caches.open(cacheName);
+        console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+        cache.put(e.request, response.clone());
+        return response;
+    })());
+});
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(caches.keys().then((keyList) => {
+        return Promise.all(keyList.map((key) => {
+            if (key === cacheName) { return; }
+            return caches.delete(key);
+        }))
+    }));
+});
